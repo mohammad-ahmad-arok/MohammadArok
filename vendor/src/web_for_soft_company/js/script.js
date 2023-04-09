48bf13cd489f57dc427527f937b7b75fa70ba7a2
mohammad-ahmad-arok <121833815+mohammad-ahmad-arok@users.noreply.github.com>
2023-04-09 05:40:14 +0300

    Add files via upload

---
+++ vendor/src/web_for_soft_company/js/script.js
@@ -0,0 +1,15 @@
+const sidebarOpen = document.querySelector('.sidebarOpen')
+body = document.querySelector('body')
+nav = document.querySelector('nav');
+
+sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});

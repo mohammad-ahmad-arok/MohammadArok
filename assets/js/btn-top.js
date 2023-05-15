908a7b14742f05875aaeeeee3511a47121c252d3
mohammad-ahmad-arok <121833815+mohammad-ahmad-arok@users.noreply.github.com>
2023-05-15 17:46:35 +0300

    Add files via upload

--- assets/js/btn-top.js
+++ assets/js/btn-top.js
@@ -2,21 +2,23 @@ const btnTop = document.querySelector('.btn-top svg');
 
 
 
-window.onscroll = () => {
-    scrollfun()
-}
 function scrollfun(params) {
-//     if (document.body.scrollTop > 500 || document.documentElement.scrollTop>500) {
-//         btnTop.style.display = 'block';
+
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop>500) {
+        btnTop.style.display = 'block';
        
-// }
-// else {
-//         btnTop.style.display = 'none';
+}
+else {
+        btnTop.style.display = 'none';
         
-    // } 
-    console.log("okj");
+    } 
+ 
 
 }
+
+window.addEventListener('scroll', () => {
+    scrollfun()
+})
 
 btnTop.addEventListener('click', () => {
     document.body.scrollTop =0;
